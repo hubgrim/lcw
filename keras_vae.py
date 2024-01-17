455dd5b109ca8e55ccd029a33814ddb7b3d3f7bf
Georg Grünert <georg.gruenert@mni.thm.de>
2024-01-17 15:06:42 +0100

    fixed and runs

--- keras_vae.py
+++ keras_vae.py
@@ -10,10 +10,12 @@ from keras import layers
 from scipy.spatial.distance import cdist
 import math
 
+tf.config.run_functions_eagerly(True)
+
 def silverman_rule_of_thumb_normal(N: int) -> float:
     return (4/(3*N))**0.4
 
-def pairwise_distances(x, y):
+def pairwise_distances(x, y = None):
     if y is None:
         y = x
     x_np = x.numpy()
@@ -23,12 +25,12 @@ def pairwise_distances(x, y):
     return distances_tf
 
 def euclidean_norm_squared(X, axis):
-    return K.sum(K.square(K.sqrt(K.sum(K.square(X), axis=axis))), axis=-1)
+    return tf.math.reduce_sum(tf.math.square(tf.math.sqrt(tf.math.reduce_sum(tf.math.square(X), axis=axis))), axis=-1)
 
 def cw_normality(X, y = None):
-    assert len(X.size()) == 2
+    assert len(X.shape) == 2
 
-    N, D = X.size()
+    N, D = X.shape
 
     if y is None:
         y = silverman_rule_of_thumb_normal(N)
@@ -36,12 +38,12 @@ def cw_normality(X, y = None):
     K1 = 1.0/(2.0*D-3.0)
 
     A1 = pairwise_distances(X)
-    A = tf.reduce_mean(1/K.sqrt(y + K1*A1))
+    A = tf.reduce_mean(1/tf.math.sqrt(y + K1*A1))
 
     B1 = euclidean_norm_squared(X, axis=1)
-    B = 2* tf.reduce_mean((1/K.sqrt(y + 0.5 + K1*B1)))
+    B = 2* tf.reduce_mean((1/tf.math.sqrt(y + 0.5 + K1*B1)))
 
-    return (1/math.sqrt(1+y)) + A - B
+    return (1/math.sqrt(1+y)) + tf.cast(A, dtype=tf.float32) - B
 
 def phi_sampling(s, D):
     return (1.0 + 4.0*s/(2.0*D-3))**(-0.5)
@@ -55,30 +57,34 @@ def cw_sampling(first_sample, second_sample, y):
     dim = np.prod(shape[1:])
     second_sample = tf.reshape(second_sample, [-1, dim])
 
-    assert len(first_sample.size()) == 2
-    assert first_sample.size() == second_sample.size()
+    assert len(first_sample.shape) == 2
+    assert first_sample.shape == second_sample.shape
 
-    _, D = first_sample.size()
+    _, D = first_sample.shape
 
     T = 1.0/(2.0*math.sqrt(math.pi*y))
 
     A0 = pairwise_distances(first_sample)
-    A = (phi_sampling(A0/(4*y), D)).mean()
+    A = tf.reduce_mean(phi_sampling(A0/(4*y), D))
 
     B0 = pairwise_distances(second_sample)
-    B = (phi_sampling(B0/(4*y), D)).mean()
+    B = tf.reduce_mean(phi_sampling(B0/(4*y), D))
 
     C0 = pairwise_distances(first_sample, second_sample)
-    C = (phi_sampling(C0/(4*y), D)).mean()
+    C = tf.reduce_mean(phi_sampling(C0/(4*y), D))
 
     return T*(A + B - 2*C)
 
 def cw_sampling_silverman(first_sample, second_sample):
-    with tf.no_grad():
-        stddev = tf.math.reduce_std(second_sample)
-        N = tf.shape(second_sample)[0]
-        gamma = silverman_rule_of_thumb_normal(N)
+    stddev = tf.math.reduce_std(second_sample)
+    N = tf.shape(second_sample)[0]
+    gamma = silverman_rule_of_thumb_normal(N)
     return cw_sampling(first_sample, second_sample, gamma)
+
+
+def cw_cost_function(x, y , z, lambda_val):
+    return tf.math.log(cw_sampling_silverman(x, y)) + lambda_val*tf.math.log(cw_normality(z))
+
 
 class Sampling(layers.Layer):
     """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
@@ -119,40 +125,35 @@ class VAE(keras.Model):
         self.decoder = decoder
         self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
         self.reconstruction_loss_tracker = keras.metrics.Mean(
-            name="reconstruction_loss"
+            name="cw_reconstruction_loss"
         )
-        self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
+        self.cw_loss_tracker = keras.metrics.Mean(name="cw_loss")
 
     @property
     def metrics(self):
         return [
             self.total_loss_tracker,
             self.reconstruction_loss_tracker,
-            self.kl_loss_tracker,
+            self.cw_loss_tracker,
         ]
 
     def train_step(self, data):
         with tf.GradientTape() as tape:
             z_mean, z_log_var, z = self.encoder(data)
             reconstruction = self.decoder(z)
-            reconstruction_loss = tf.reduce_mean(
-                tf.reduce_sum(
-                    keras.losses.binary_crossentropy(data, reconstruction),
-                    axis=(1, 2),
-                )
-            )
-            kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
-            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
-            total_loss = reconstruction_loss + kl_loss
+            cw_reconstruction_loss = tf.math.log(cw_sampling_silverman(data, reconstruction))
+            lambda_val = 1
+            cw_loss = lambda_val*tf.math.log(cw_normality(z))
+            total_loss = tf.cast(cw_reconstruction_loss, dtype=tf.float32) + cw_loss
         grads = tape.gradient(total_loss, self.trainable_weights)
         self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
         self.total_loss_tracker.update_state(total_loss)
-        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
-        self.kl_loss_tracker.update_state(kl_loss)
+        self.reconstruction_loss_tracker.update_state(cw_reconstruction_loss)
+        self.cw_loss_tracker.update_state(cw_loss)
         return {
             "loss": self.total_loss_tracker.result(),
-            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
-            "kl_loss": self.kl_loss_tracker.result(),
+            "cw_reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "cw_loss": self.cw_loss_tracker.result(),
         }
 
 
