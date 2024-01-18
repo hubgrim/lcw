fd44f8f68df879f863cfc81dc6d1e136d09e0163
Georg Grünert <georg.gruenert@mni.thm.de>
2024-01-18 14:32:15 +0100

    plots

--- keras_vae.py
+++ keras_vae.py
@@ -9,6 +9,8 @@ from keras import backend as K
 from keras import layers
 from scipy.spatial.distance import cdist
 import math
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 tf.config.run_functions_eagerly(True)
 
@@ -165,3 +167,57 @@ vae = VAE(encoder, decoder)
 vae.compile(optimizer=keras.optimizers.Adam())
 vae.fit(mnist_digits, epochs=30, batch_size=128)
 
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+filename = f'_{timestamp}.png'
+
+def plot_latent_space(vae, filename, n=30, figsize=15):
+    # display a n*n 2D manifold of digits
+    digit_size = 28
+    scale = 1.0
+    figure = np.zeros((digit_size * n, digit_size * n))
+    # linearly spaced coordinates corresponding to the 2D plot
+    # of digit classes in the latent space
+    grid_x = np.linspace(-scale, scale, n)
+    grid_y = np.linspace(-scale, scale, n)[::-1]
+
+    for i, yi in enumerate(grid_y):
+        for j, xi in enumerate(grid_x):
+            z_sample = np.array([[xi, yi]])
+            x_decoded = vae.decoder.predict(z_sample, verbose=0)
+            digit = x_decoded[0].reshape(digit_size, digit_size)
+            figure[
+                i * digit_size : (i + 1) * digit_size,
+                j * digit_size : (j + 1) * digit_size,
+            ] = digit
+
+    plt.figure(figsize=(figsize, figsize))
+    start_range = digit_size // 2
+    end_range = n * digit_size + start_range
+    pixel_range = np.arange(start_range, end_range, digit_size)
+    sample_range_x = np.round(grid_x, 1)
+    sample_range_y = np.round(grid_y, 1)
+    plt.xticks(pixel_range, sample_range_x)
+    plt.yticks(pixel_range, sample_range_y)
+    plt.xlabel("z[0]")
+    plt.ylabel("z[1]")
+    plt.imshow(figure, cmap="Greys_r")
+    plt.savefig("samples"+filename, dpi=300)
+
+
+plot_latent_space(vae, filename)
+
+def plot_label_clusters(vae, data, labels, filename):
+    # display a 2D plot of the digit classes in the latent space
+    z_mean, _, _ = vae.encoder.predict(data, verbose=0)
+    plt.figure(figsize=(12, 10))
+    plt.scatter(z_mean[:, 0], z_mean[:, 1], c=labels)
+    plt.colorbar()
+    plt.xlabel("z[0]")
+    plt.ylabel("z[1]")
+    plt.savefig("latent_distribution"+filename, dpi=300)
+
+
+(x_train, y_train), _ = keras.datasets.mnist.load_data()
+x_train = np.expand_dims(x_train, -1).astype("float32") / 255
+
+plot_label_clusters(vae, x_train, y_train, filename)
